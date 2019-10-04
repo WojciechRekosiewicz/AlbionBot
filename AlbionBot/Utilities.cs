02a0818e93589f5ffc63b32ad012f0627999407a
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-10-05 00:50:52 +0200

    start creating utilities to read json alerts.json

---
+++ AlbionBot/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AlbionBot
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+
+
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLan/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+    }
+}
