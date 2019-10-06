fe883cc44ea0502c2afb78eabed4359247e70166
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-10-06 19:53:25 +0200

    modify main to start the bot

--- AlbionBot/Program.cs
+++ AlbionBot/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace AlbionBot
 {
@@ -18,7 +19,22 @@ namespace AlbionBot
 
         public async Task StartAsync()
         {
-
+            if (Config.bot.token == "" || Config.bot.token == null) return;
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose                            
+            });
+            _client.Log += Log;         
+            await _client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await _client.StartAsync();
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+            await Task.Delay(-1);
         }
+
+        private async Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);            
+         }
     }
 }
