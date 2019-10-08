e9ab7c499db5aeabe95c3734dfa3400dd654243a
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-10-09 00:21:45 +0200

    UserAccounts done

--- AlbionBot/Core/DataStorage.cs
+++ AlbionBot/Core/DataStorage.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using AlbionBot.Core.UserAccounts;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AlbionBot.Core
 {
-    class DataStorage
+    public static class DataStorage
     {
+        public static void SaveUserAccounts(IEnumerable<UserAccount> accounts, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(accounts);
+            File.WriteAllText(filePath, json);
+        }
 
+        public static IEnumerable<UserAccount> LoadUserAccounts(string filePath)
+        {
+            if (!File.Exists(filePath)) return null;
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<UserAccount>>(json);
+        }
+
+        public static bool SaveExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
     }
 }
--- AlbionBot/Core/UserAccounts/UserAccounts.cs
+++ AlbionBot/Core/UserAccounts/UserAccounts.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,60 @@ namespace AlbionBot.Core.UserAccounts
 {
     public static class UserAccounts
     {
+        private static List<UserAccount> accounts;
+
+        private static string accountsFile = "Resources/accounts.json";
+
         static UserAccounts()
         {
+            if(DataStorage.SaveExists(accountsFile))
+            {
+                accounts = DataStorage.LoadUserAccounts(accountsFile).ToList();
+            }
+            else
+            {
+                accounts = new List<UserAccount>();
+                SaveAccounts();
+            }
+        }
 
+        public static void SaveAccounts()
+        {
+            DataStorage.SaveUserAccounts(accounts, accountsFile);
+        }
+
+        public static UserAccount GetAccount(SocketUser user)
+        {
+            return GetOrCreateAccount(user.Id);
+
+        }
+
+        private static UserAccount GetOrCreateAccount(ulong id)
+        {
+            var result = from a in accounts
+                         where a.ID == id
+                         select a;
+
+            var account = result.FirstOrDefault();
+
+
+            if (account == null) account = CreateUserAccount(id);        
+            return account;
+        }
+
+        public static UserAccount CreateUserAccount(ulong id)
+        {
+            var newAccount = new UserAccount()
+            {
+                ID = id,
+                Points = 10,
+                XP = 0,
+                TimeSpend = 0
+            };
+
+            accounts.Add(newAccount);
+            SaveAccounts();
+            return newAccount;
         }
     }
 }
