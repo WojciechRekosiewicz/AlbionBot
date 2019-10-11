d4a54e3fa6f9dcd0919abe24113a9cb6ff4d442a
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-10-11 03:24:29 +0200

    start adding method to check if player is in guild

--- AlbionBot/Core/RepeatingTimer.cs
+++ AlbionBot/Core/RepeatingTimer.cs
@@ -1,7 +1,9 @@
 ﻿using Discord.WebSocket;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -26,11 +28,33 @@ namespace AlbionBot.Core
                 AutoReset = true,
                 Enabled = true
             };
-            loopingTimer.Elapsed += OnTimerTicked;         
+            //loopingTimer.Elapsed += OnTimerTicked;
+            loopingTimer.Elapsed += CheckGuild;
 
             return Task.CompletedTask;
 
         }
+
+        private static async void CheckGuild(object sender, ElapsedEventArgs e)
+        {
+            if (Global.Client == null)
+            {
+            string json = "";
+            using (WebClient client = new WebClient())
+            {
+                json = client.DownloadString("https://gameinfo.albiononline.com/api/gameinfo/players/gV5ro9GCTq-mF-yr-WierA");
+            }
+
+            var dataObject = JsonConvert.DeserializeObject<dynamic>(json);
+
+            //var name = dataObject.results[3].AverageItemPower.ToString();
+            var name = dataObject.Name.ToString();
+            Console.WriteLine("checked");
+            }
+            //await Context.Channel.SendMessageAsync($"Nick : {name}");
+
+        }
+
 
         private static async void OnTimerTicked(object sender, ElapsedEventArgs e)
         {
